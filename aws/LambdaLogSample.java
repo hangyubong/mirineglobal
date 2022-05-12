4684e10e3db4609faacd27db4bb855620fcd4a4f
hangyubong <97740702+hangyubong@users.noreply.github.com>
2022-05-12 16:57:37 +0900

    Add files via upload

---
+++ aws/LambdaLogSample.java
@@ -0,0 +1,20 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LambdaLogSample implements RequestHandler<S3Event, Object> {
+
+    static final Logger logger = LogManager.getLogger(LambdaLogSample.class);
+
+    @Override
+    public Object handleRequest(S3Event input, Context context) {
+    	String response = new String("Hello Lambda!!");
+        logger.info(logger.getLevel());
+        return response;
+    }
+}
